ba04bf36490dd227f57561db816e47776e6d3dd8
sergeycccv <sergey.s.saveliev@yandex.ru>
2024-04-18 11:50:47 +0700

    Глава 0.02

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,17 @@
+from flask import render_template
 from app import app
 @app.route('/')
-@app.route('/index')
+#@app.route('/index')
 def index():
-    return "Привет, Мир!"
+    user = {'username': 'Сергей'}
+    posts = [
+        {
+        'author': {'username': 'Алексей'},
+        'body': 'Это сообщение от Алексей!'
+        },
+        {
+        'author': {'username': 'Дарья'},
+        'body': 'А это сообщение от Дарьи!'
+        }
+    ]
+    return render_template('index.html', title='Главная страница', user=user, posts=posts)
