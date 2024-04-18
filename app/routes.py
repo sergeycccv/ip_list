4f8463c279cdfd2eb3e35af01ed69ff0384079c9
sergeycccv <sergey.s.saveliev@yandex.ru>
2024-04-18 13:59:24 +0700

    Глава 0.03

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,8 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
+from app.forms import LoginForm
+
+
 @app.route('/')
 #@app.route('/index')
 def index():
@@ -15,3 +18,12 @@ def index():
         }
     ]
     return render_template('index.html', title='Главная страница', user=user, posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(f'Пользователь {form.username.data} успешно зашёл, remember_me = {form.remember_me.data}')
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Вход в систему', form=form)
