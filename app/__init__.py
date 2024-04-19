e2825df10c86ca0dadfc082bd39e2ed8600a7e66
sergeycccv <sergey.s.saveliev@yandex.ru>
2024-04-19 13:26:42 +0700

    Добавлно отправка email при ошибках

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from logging.handlers import RotatingFileHandler
+from logging.handlers import RotatingFileHandler, SMTPHandler
 import logging
 import os
 from flask import Flask
@@ -17,6 +17,22 @@ login.login_view = 'login'
 login.login_message = 'Пожалуйста, авторизуйтесь для доступа к этой странице.'
 
 if not app.debug:
+
+    if app.config['MAIL_SERVER']:
+        auth = None
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+        secure = None
+        if app.config['MAIL_USE_TLS']:
+            secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+            fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+            toaddrs=app.config['ADMINS'], subject='Ошибка в IP-address',
+            credentials=auth, secure=secure)
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
+
     if not os.path.exists('logs'):
         os.mkdir('logs')
     file_handler = RotatingFileHandler('logs/ip_list.log', maxBytes=10240,
