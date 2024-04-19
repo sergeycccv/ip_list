09ef5587dd935c42924ddb2d7d0c5032057ec8e0
sergeycccv <sergey.s.saveliev@yandex.ru>
2024-04-19 12:39:05 +0700

    Глава 0.07

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,6 @@
+from logging.handlers import RotatingFileHandler
+import logging
+import os
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
@@ -13,4 +16,16 @@ login = LoginManager(app)
 login.login_view = 'login'
 login.login_message = 'Пожалуйста, авторизуйтесь для доступа к этой странице.'
 
-from app import routes, models
+if not app.debug:
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/ip_list.log', maxBytes=10240,
+                                       backupCount=10)
+    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('IP_address запущена')
+
+from app import routes, models, errors
--- app/forms.py
+++ app/forms.py
@@ -38,3 +38,14 @@ class EditProfileForm(FlaskForm):
     username = StringField('Пользователь', validators=[DataRequired()])
     about_me = TextAreaField('Обо мне', validators=[Length(min=0, max=140)])
     submit = SubmitField('Сохранить')
+
+    def __init__(self, original_username, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.original_username = original_username
+
+    def validate_username(self, username):
+        if username.data != self.original_username:
+            user = db.session.scalar(sa.select(User).where(
+                User.username == self.username.data))
+            if user is not None:
+                raise ValidationError('Это имя пользователя уже занято')
