ab26e3549d5b66504e3c792538fbef064ddaa0f9
sergeycccv <sergey.s.saveliev@yandex.ru>
2024-04-22 09:11:50 +0700

    Глава 0.09

--- app/routes.py
+++ app/routes.py
@@ -9,6 +9,7 @@ from datetime import datetime, timezone
 import sqlalchemy as sa
 from app.forms import EmptyForm
 
+
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 @login_required
@@ -20,8 +21,14 @@ def index():
         db.session.commit()
         flash('Ваш пост опубликован!')
         return redirect(url_for('index'))
-    posts = db.session.scalars(current_user.following_posts()).all()
-    return render_template("index.html", title='Главная страница', form=form, posts=posts)
+    page = request.args.get('page', 1, type=int)
+    posts = db.paginate(current_user.following_posts(), page=page,
+                        per_page=app.config['POSTS_PER_PAGE'], error_out=False)
+    next_url = url_for('index', page=posts.next_num) if posts.has_next else None
+    prev_url = url_for('index', page=posts.prev_num) if posts.has_prev else None
+    return render_template('index.html', title='Главная страница', form=form,
+                           posts=posts.items, next_url=next_url,
+                           prev_url=prev_url)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -68,12 +75,19 @@ def logout():
 @login_required
 def user(username):
     user = db.first_or_404(sa.select(User).where(User.username == username))
-    posts = [
-        {'author': user, 'body': 'Test post #1'},
-        {'author': user, 'body': 'Test post #2'}
-    ]
+    page = request.args.get('page', 1, type=int)
+    query = user.posts.select().order_by(Post.timestamp.desc())
+    posts = db.paginate(query, page=page,
+                        per_page=app.config['POSTS_PER_PAGE'],
+                        error_out=False)
+    next_url = url_for('user', username=user.username, page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('user', username=user.username, page=posts.prev_num) \
+        if posts.has_prev else None
     form = EmptyForm()
-    return render_template('user.html', user=user, posts=posts, form=form)
+    return render_template('user.html', user=user, posts=posts.items,
+                           next_url=next_url, prev_url=prev_url, form=form)
+
 
 # Запрос времени после каждого обращения к БД
 @app.before_request
@@ -144,6 +158,11 @@ def unfollow(username):
 @app.route('/explore')
 @login_required
 def explore():
+    page = request.args.get('page', 1, type=int)
     query = sa.select(Post).order_by(Post.timestamp.desc())
-    posts = db.session.scalars(query).all()
-    return render_template('index.html', title='Исследовать', posts=posts)
+    posts = db.paginate(query, page=page,
+                        per_page=app.config['POSTS_PER_PAGE'], error_out=False)
+    next_url = url_for('explore', page=posts.next_num) if posts.has_next else None
+    prev_url = url_for('explore', page=posts.prev_num) if posts.has_prev else None
+    return render_template("index.html", title='Исследовать', posts=posts.items,
+                           next_url=next_url, prev_url=prev_url)
--- config.py
+++ config.py
@@ -14,3 +14,5 @@ class Config:
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['sergey.s.saveliev@yandex.ru']
+
+    POSTS_PER_PAGE = 3
